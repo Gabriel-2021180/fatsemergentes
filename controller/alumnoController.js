bf0b28f7a856902613491ee19435f2b1d1200b8e
RDYS0Raiden <103591207+RDYS0Raiden@users.noreply.github.com>
2023-10-12 15:46:37 -0400

    Update alumnoController.js

--- controller/alumnoController.js
+++ controller/alumnoController.js
@@ -1,36 +1,47 @@
-const LogicaNegocios = require('../Logica De Negocio/datosLN');
+const fetch = require('node-fetch');
+const axios = require('axios');
+const ip = require('ip');
 
-exports.datosAgregados= async (req, res) => {
-    
-  const {
+module.exports.mostrar = (req, res) => {
+  const ip = require('ip');
+  const clientIp = ip.address(); // Obtén la dirección IP privada del cliente
+  res.render('index', { "clientIp": clientIp });
+};
+
+exports.enviar = async (req, res) => {
+  try {
+    // Obtén los valores de los campos del formulario
+    const frase = req.body.frase;
+    const reaccion = req.body.btnReaccion;
+    const colorfondo = req.body.colorFondo;
+    const colorLetra = req.body.colorLetra;
+    const ip = req.body.ip;
+    const sexo =req.body.sexo; // Asegúrate de que este campo se llame 'ip' en el formulario
+
+    // Define los datos que deseas enviar a la API
+    const datos = {
       frase,
       reaccion,
       colorfondo,
       colorLetra,
       ip,
-      sexo,
-      //cantidadClicks,
-    } = req.body;
+      sexo
+    };
 
+    // Realiza una solicitud POST a la API con los datos
+    const response = await axios.post('https://apicitaparaemergentes.onrender.com/crear', datos);
+
+    // Maneja la respuesta de la API como desees
     
-  try {
-      const resultado = await LogicaNegocios.datosAgregados(
-        frase,
-        reaccion,
-        colorfondo,
-        colorLetra,
-        ip,
-        sexo,
-        //cantidadClicks,
-      );
 
-      // El controlador solo necesita comprobar el mensaje de éxito
-      if (resultado === '¡Registro guardado correctamente!') {
-        res.json({ mensaje: resultado });
-      } else {
-        res.json({ mensaje: resultado });
-      }
-    } catch (error) {
-      res.json({ mensaje: error.message });
+    // Responde a la solicitud en tu controlador de aplicación
+    if (response.data.mensaje === '¡Registro guardado correctamente!') {
+      res.redirect(302, '/');
+    } else {
+      res.status(200).json({ mensaje: response.data.mensaje });
     }
+  } catch (error) {
+    console.error('Error al enviar la solicitud POST a la API:', error);
+    res.status(500).json({ error: 'Error en la solicitud POST a la API' });
+  }
 };
